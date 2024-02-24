369da0dedf8ab3fe4aef6a184bee6cba70aba1dc
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-24 18:02:59 +0530

    chore: form skeleton

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import * as z from "zod";
 import { Category, Companion } from "@prisma/client";
+
+const formSchema = z.object({});
 
 interface CompanionFormProps {
   initialData: Companion | null;
