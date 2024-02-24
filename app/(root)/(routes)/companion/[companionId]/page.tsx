e0110d048c0bad166a4efe62070b14d21513e100
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-24 17:46:41 +0530

    feat: companion id page

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,9 +1,15 @@
-const CompanionIdPage = () => {
-    return ( 
-        <div>
-            Hello Companion Id
-        </div>
-     );
+interface CompanionIdPageProps {
+  params: {
+    companionId: string;
+  };
 }
- 
+
+const CompanionIdPage = async ({ params }: CompanionIdPageProps) => {
+  return (
+    // TODO: Check Subscription
+    
+    <div>Hello Companion Id</div>
+  );
+};
+
 export default CompanionIdPage;
