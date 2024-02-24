90363a15af0484ae0b29fa2ec39d2765e307b301
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-24 17:52:22 +0530

    feat: companion-form skeleton

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -1,7 +1,14 @@
-const CompanionForm = () => {
-    return ( <div>
-        CompanionForm
-    </div> );
+"use client";
+
+import { Category, Companion } from "@prisma/client";
+
+interface CompanionFormProps {
+  initialData: Companion | null;
+  categories: Category[];
 }
- 
+
+const CompanionForm = ({ initialData, categories }: CompanionFormProps) => {
+  return <div>CompanionForm</div>;
+};
+
 export default CompanionForm;
