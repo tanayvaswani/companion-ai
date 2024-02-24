fd0d036207d57290340880c8ce8cf4c089868485
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-24 18:56:23 +0530

    feat: form

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -5,6 +5,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Category, Companion } from "@prisma/client";
 import { useForm } from "react-hook-form";
 
+import { Form } from "@/components/ui/form";
+
 const formSchema = z.object({
   name: z.string().min(1, {
     message: "Name is required!",
@@ -46,7 +48,30 @@ const CompanionForm = ({ initialData, categories }: CompanionFormProps) => {
 
   const isLoading = form.formState.isSubmitting;
 
-  return <div>CompanionForm</div>;
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="h-full space-y-2 max-w-3xl mx-auto">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 pb-10"
+        >
+          <div className="space-y-2 w-full col-span-2">
+            <div>
+              <h3 className="text-lg font-medium">General Information</h3>
+
+              <p className="text-sm text-muted-foreground">
+                General information about your companion
+              </p>
+            </div>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default CompanionForm;
