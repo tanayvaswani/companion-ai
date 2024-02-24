54374fdbde71f540ab7946ce3fa4c34ba866e402
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-24 15:13:39 +0530

    feat: mobile-sidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,9 +1,20 @@
+import { Menu } from "lucide-react";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "@/components/sidebar";
+
 const MobileSidebar = () => {
-    return (  
-        <div>
-            Mobile Sidebar
-        </div>
-    );
-}
- 
+  return (
+    <Sheet>
+      <SheetTrigger className="block md:hidden pr-4">
+        <Menu />
+      </SheetTrigger>
+
+      <SheetContent side={"left"} className="p-0 bg-secondary pt-10 w-32">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
 export default MobileSidebar;
