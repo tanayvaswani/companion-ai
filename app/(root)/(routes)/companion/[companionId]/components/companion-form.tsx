ac0726a305a170bac95f6fc49623af5dcfd63b7f
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-24 19:41:23 +0530

    feat: form

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -5,7 +5,14 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Category, Companion } from "@prisma/client";
 import { useForm } from "react-hook-form";
 
-import { Form } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -59,7 +66,7 @@ const CompanionForm = ({ initialData, categories }: CompanionFormProps) => {
           onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-8 pb-10"
         >
-          <div className="space-y-2 w-full col-span-2">
+          <div className="space-y-2 w-full">
             <div>
               <h3 className="text-lg font-medium">General Information</h3>
 
@@ -67,7 +74,20 @@ const CompanionForm = ({ initialData, categories }: CompanionFormProps) => {
                 General information about your companion
               </p>
             </div>
+
+            <Separator className="bg-primary/10" />
           </div>
+
+          <FormField
+            name="src"
+            render={({ field }) => (
+              <FormItem className="flex flex-col items-center justify-center space-y-4">
+                <FormControl>Image Upload</FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </form>
       </Form>
     </div>
