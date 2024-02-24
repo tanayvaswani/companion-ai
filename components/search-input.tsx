3885a10e939d67292816556294a2df0f851de16b
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-24 15:28:58 +0530

    feat: search input

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,10 +1,20 @@
 "use client";
 
 import { Search } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
 
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 const SearchInput = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const categoryId = searchParams.get("categoryId");
+  const name = searchParams.get("name");
+
+    
+
   return (
     <div className="relative">
       <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
