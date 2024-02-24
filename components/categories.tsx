d8b44a93631e976ff381292c493e1c28b3dc22b7
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-24 17:04:47 +0530

    feat: categories

--- components/categories.tsx
+++ components/categories.tsx
@@ -36,7 +36,8 @@ export const Categories = ({ data }: CategoriesProps) => {
     <div className="w-full overflow-x-auto space-x-2 flex p-1">
       <button
         onClick={() => onClick(undefined)}
-        className={cn(`
+        className={cn(
+          `
           flex items-center text-center text-xs md:text-sm
           px-2 md:px-4
           py-2 md:py-3
@@ -44,7 +45,9 @@ export const Categories = ({ data }: CategoriesProps) => {
           bg-primary/10
           hover:opacity-75
           transition
-      `)}
+      `,
+          !categoryId ? "bg-primary/25" : "bg-primary/10"
+        )}
       >
         Newest
       </button>
@@ -54,15 +57,16 @@ export const Categories = ({ data }: CategoriesProps) => {
           <button
             onClick={() => onClick(item.id)}
             key={item.id}
-            className={cn(`
-              flex items-center text-center text-xs md:text-sm
+            className={cn(
+              `flex items-center text-center text-xs md:text-sm
               px-2 md:px-4
               py-2 md:py-3
               rounded-md
               bg-primary/10
               hover:opacity-75
-              transition
-          `)}
+              transition`,
+              item.id === categoryId ? "bg-primary/25" : "bg-primary/10"
+            )}
           >
             {item.name}
           </button>
