c886afe82c3ac08a9f7c101e8728b0cccaf8ff63
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-24 16:21:08 +0530

    feat: seed script

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -4,10 +4,36 @@ const db = new PrismaClient();
 
 async function main() {
   try {
-
+    await db.categories.createMany({
+      data: [
+        {
+          name: "Famous People",
+        },
+        {
+          name: "Movies & TV",
+        },
+        {
+          name: "Musicians",
+        },
+        {
+          name: "Games",
+        },
+        {
+          name: "Animals",
+        },
+        {
+          name: "Philosophy",
+        },
+        {
+          name: "Scientists",
+        },
+      ],
+    });
   } catch (error) {
     console.error("Error seeding default categories: ", error);
   } finally {
     await db.$disconnect();
   }
 }
+
+main();
