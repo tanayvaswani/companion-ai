3dc30983daa550eb7483a302a44a8417d5a1c227
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-24 19:51:35 +0530

    feat: cloudinary skeleton

---
+++ components/image-upload.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface ImageUploadProps {
+  value: string;
+  onChange: (src: string) => void;
+  disabled?: boolean;
+}
+
+export const ImageUpload = ({
+  value,
+  onChange,
+  disabled,
+}: ImageUploadProps) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return <div className="">
+    
+  </div>;
+};
