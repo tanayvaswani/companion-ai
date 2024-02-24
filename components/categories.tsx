32ce88f4d24979c16bf1b4c6db6ad7b05f882da4
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-24 16:55:58 +0530

    feat: categories

--- components/categories.tsx
+++ components/categories.tsx
@@ -2,10 +2,33 @@
 
 import { Category } from "@prisma/client";
 
+import { cn } from "@/lib/utils";
+
 interface CategoriesProps {
   data: Category[];
 }
 
-export const Categories = () => {
-  return <div>Categories</div>;
+export const Categories = ({ data }: CategoriesProps) => {
+  return (
+    <div className="w-full overflow-x-auto space-x-2 flex p-1">
+      {data.map((item) => {
+        return (
+          <button
+            key={item.id}
+            className={cn(`
+                flex items-center text-center text-xs md:text-sm
+                px-2 md:px-4
+                py-2 md:py-3
+                rounded-md
+                bg-primary/10
+                hover:opacity-75
+                transition
+            `)}
+          >
+            {item.name}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
