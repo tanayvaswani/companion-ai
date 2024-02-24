ae87aede9664b8c9bc67182e32132fc0b7ba61f4
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-24 16:50:06 +0530

    feat: categories skeleton

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,17 @@
+import { Categories } from "@/components/categories";
 import SearchInput from "@/components/search-input";
+import prismadb from "@/lib/prismadb";
 
-const RootPage = () => {
+const RootPage = async () => {
+  const categories = await prismadb.category.findMany({
+    where: {},
+  });
+
   return (
     <main className="h-full p-4 space-y-2">
       <SearchInput />
+
+      <Categories data={categories} />
     </main>
   );
 };
---
+++ components/categories.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Category } from "@prisma/client";
+
+interface CategoriesProps {
+  data: Category[];
+}
+
+export const Categories = () => {
+  return <div>Categories</div>;
+};
