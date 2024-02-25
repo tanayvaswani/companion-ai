87ad3646890fdacce33a652c938fbb1d88b1c86a
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-25 14:59:00 +0530

    feat: POST companion api

---
+++ app/api/companion/route.ts
@@ -0,0 +1,30 @@
+import { currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const user = await currentUser();
+    const { src, name, description, instructions, seed, categoryId } = body;
+
+    if (!user || !user.id || !user.firstName) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (
+      !src ||
+      !name ||
+      !description ||
+      !instructions ||
+      !seed ||
+      !categoryId
+    ) {
+      return new NextResponse("Missing fields", { status: 400 });
+    }
+
+    // TODO: Check for subscription
+  } catch (error) {
+    console.log("[COMPANION_POST]", error);
+    return new NextResponse("Internal Server Error!", { status: 500 });
+  }
+}
