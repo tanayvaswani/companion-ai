0ac5de352b75cb289d5c2d4a6d274b0363bc4a0f
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-25 19:11:34 +0530

    refac: chat header skeleton

--- app/(chat)/(routes)/chat/[chatId]/_components/chat-header.tsx
+++ app/(chat)/(routes)/chat/[chatId]/_components/chat-header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, MessagesSquare } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
@@ -31,7 +31,23 @@ const ChatHeader = ({ companion }: ChatHeaderProps) => {
         >
           <ChevronLeft className="h-8 w-8" />
         </Button>
+
         <BotAvatar src={companion.src} />
+
+        <div className="flex flex-col gap-y-1">
+          <div className="flex items-center gap-x-2">
+            <p className="font-bold">{companion.name}</p>
+
+            <div className="flex items-center text-xs text-muted-foreground">
+              <MessagesSquare className="h-3 w-3 mr-1" />
+              {companion._count.messages}
+            </div>
+          </div>
+
+          <p className="text-xs text-muted-foreground">
+            Created by {companion.userName}
+          </p>
+        </div>
       </div>
     </div>
   );
