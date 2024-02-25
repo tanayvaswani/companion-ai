045751bcf9bd2e82268900a481ce5235b2a12307
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-25 17:54:00 +0530

    feat: Companions on homepage

--- components/companions.tsx
+++ components/companions.tsx
@@ -1,5 +1,9 @@
 import { Companion } from "@prisma/client";
 import Image from "next/image";
+import Link from "next/link";
+import { MessageSquare } from "lucide-react";
+
+import { Card, CardFooter, CardHeader } from "@/components/ui/card";
 
 interface CompanionProps {
   data: (Companion & {
@@ -22,7 +26,42 @@ const Companions = ({ data }: CompanionProps) => {
     );
   }
 
-  return <div>Companions</div>;
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 pb-10">
+      {data.map((comp) => (
+        <Card
+          key={comp.id}
+          className="bg-primary/10 rounded-xl cursor-pointer hover:opacity-75 transition border-0"
+        >
+          <Link href={`/chat/${comp.id}`}>
+            <CardHeader className="flex items-center justify-center text-center text-muted-foreground">
+              <div className="relative  w-32 h-32">
+                <Image
+                  src={comp.src}
+                  fill
+                  className="rounded-xl object-cover"
+                  alt="Companion"
+                />
+              </div>
+
+              <p className="font-bold">{comp.name}</p>
+
+              <p className="text-xs">{comp.description}</p>
+            </CardHeader>
+
+            <CardFooter className="flex items-center justify-between text-xs text-muted-foreground">
+              <p className="lowercase">@{comp.userName}</p>
+
+              <div className="flex items-center">
+                <MessageSquare className="w-3 h-3 mr-1" />
+                {comp._count.messages}
+              </div>
+            </CardFooter>
+          </Link>
+        </Card>
+      ))}
+    </div>
+  );
 };
 
 export default Companions;
