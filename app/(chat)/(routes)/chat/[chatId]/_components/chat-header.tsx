4618d2bed0229b91c88462db73e37b9e92fc93ad
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-25 19:03:58 +0530

    feat: Chat Header

--- app/(chat)/(routes)/chat/[chatId]/_components/chat-header.tsx
+++ app/(chat)/(routes)/chat/[chatId]/_components/chat-header.tsx
@@ -1,22 +1,38 @@
-'use client';
+"use client";
 
+import { ChevronLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
 import { Companion, Message } from "@prisma/client";
 
 interface ChatHeaderProps {
-    companion: Companion & {
-        messages: Message[];
-        _count: {
-            messages: number;
-        }
-    }
-} 
-
-const ChatHeader = ({companion}: ChatHeaderProps) => {
-    return ( 
-        <div>
-            Chat Header Props
-        </div>
-     );
+  companion: Companion & {
+    messages: Message[];
+    _count: {
+      messages: number;
+    };
+  };
 }
- 
+
+const ChatHeader = ({ companion }: ChatHeaderProps) => {
+  const router = useRouter();
+
+  return (
+    <div className="flex w-full justify-between items-center border-b border-primary/10 pb-4">
+      <div className="flex gap-x-2 items-center">
+        <Button
+          size={"icon"}
+          variant={"ghost"}
+          onClick={() => {
+            router.back();
+          }}
+        >
+          <ChevronLeft className="h-8 w-8" />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
 export default ChatHeader;
